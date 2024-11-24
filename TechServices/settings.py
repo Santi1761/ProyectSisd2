b135e98cc7f80f874b416c5e596f707b40d1269b
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2024-11-24 01:53:05 -0500

    fix: deploy

--- TechServices/settings.py
+++ TechServices/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret! Clave secreta para la aplicacion, se cambia luego
-SECRET_KEY = os.environ.get('SECRET_KEY'), default='your secret key'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production! si estamos en modo de desarrollo, false para produccion 
 DEBUG = 'RENDER' not in os.environ
