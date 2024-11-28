e47f8e116681d6f4c97b780eec19507bcac195ea
Ing-Daron11 <daronmercadog@gmail.com>
2024-11-27 23:45:36 -0500

    feat: add css to contract_list

--- TechServices/settings.py
+++ TechServices/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATICFILES_DIRS = [BASE_DIR / "static",]
 
 
 
